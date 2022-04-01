be6af3f75495bc051847acf4f170d848bf79297f
Bello <87645100+sofianbello@users.noreply.github.com>
2022-04-01 09:28:12 +0200

    Add files via upload

---
+++ Lessons/04-webpack/src/script.js
@@ -0,0 +1,35 @@
+import './style.css'
+import * as THREE from 'three'
+
+console.log(THREE)
+
+// Scene
+const scene = new THREE.Scene();
+
+// Red Cube
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add( mesh )
+
+// Camera
+
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.x = 1.5;
+camera.rotation.y = 0.5;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector('.webgl');
+console.log(canvas)
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
