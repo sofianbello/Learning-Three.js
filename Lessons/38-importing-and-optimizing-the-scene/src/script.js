5c23a4873f25342f1efcdca2ec4c13e7a177e60b
sofianbello <safian.bello@haw-hamburg.de>
2022-05-23 19:15:08 +0200

    last lession started

--- Lessons/38-importing-and-optimizing-the-scene/src/script.js
+++ Lessons/38-importing-and-optimizing-the-scene/src/script.js
@@ -4,11 +4,14 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import firefliesVertexShader from './shaders/fireflies/vertex.glsl'
+import firefliesFragmentShader from './shaders/fireflies/fragment.glsl'
 
 /**
  * Base
  */
 // Debug
+const debugObject = {}
 const gui = new dat.GUI({
     width: 400
 })
@@ -33,15 +36,85 @@ dracoLoader.setDecoderPath('draco/')
 const gltfLoader = new GLTFLoader()
 gltfLoader.setDRACOLoader(dracoLoader)
 
+
 /**
- * Object
+ * Materials
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+
+// Baked Material
+const bakedTexture = textureLoader.load('baked.jpg')
+bakedTexture.flipY = false
+bakedTexture.encoding = THREE.sRGBEncoding
+const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture })
+
+// PortalLight Material
+const portalLightMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff })
+
+// PoleLight Material
+const poleLightMaterial = new THREE.MeshBasicMaterial({ color: 0xf993fb })
+
+gui.addColor(portalLightMaterial, 'color').name('PortalLight')
+gui.addColor(poleLightMaterial, 'color').name('PoleLight')
+
+/**
+ * Model
+ */
+
+gltfLoader.load(
+    'portal.glb', // Path by default is set to 'static' this can also be written as './/portal.glb'
+    (gltf) =>
+    {
+
+        const bakedMesh = gltf.scene.children.find(child => child.name === 'baked')
+        const portalLightMesh = gltf.scene.children.find(child => child.name === 'Portal')
+        const lampAMesh = gltf.scene.children.find(child => child.name === 'poleLightA')
+        const lampBMesh = gltf.scene.children.find(child => child.name === 'poleLightB')
+        const lampCMesh = gltf.scene.children.find(child => child.name === 'poleLightC')
+        const lampDMesh = gltf.scene.children.find(child => child.name === 'poleLightD')
+
+        bakedMesh.material = bakedMaterial
+        portalLightMesh.material = portalLightMaterial
+        lampAMesh.material = poleLightMaterial
+        lampBMesh.material = poleLightMaterial
+        lampCMesh.material = poleLightMaterial
+        lampDMesh.material = poleLightMaterial
+
+        scene.add(gltf.scene);
+    }
 )
 
-scene.add(cube)
+/**
+ * Fireflies
+ */
+
+// Geometriy
+const firefliesGeometry = new THREE.BufferGeometry()
+const firefliesCount = 20
+const positionArray = new Float32Array(firefliesCount * 3)
+
+for(let i = 0; i < firefliesCount; i++)
+{
+    positionArray[i * 3 + 0 ] = (Math.random() - 0.5) * 4
+    positionArray[i * 3 + 1 ] = Math.random() * 2.5
+    positionArray[i * 3 + 2 ] = (Math.random() - 0.5) * 4
+}
+
+firefliesGeometry.setAttribute('position', new THREE.BufferAttribute(positionArray, 3))
+
+// Material
+const firefliesMaterial = new THREE.ShaderMaterial({ 
+    uniforms:
+    {
+        uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) }
+    },
+
+    vertexShader: firefliesVertexShader, 
+    fragmentShader: firefliesFragmentShader
+})
+
+// Points
+const fireflies = new THREE.Points(firefliesGeometry, firefliesMaterial)
+scene.add(fireflies)
 
 /**
  * Sizes
@@ -64,6 +137,9 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    // Update Fireflies
+    firefliesMaterial.uniforms.uPixelRatio.value = Math.min(window.devicePixelRatio, 2)
 })
 
 /**
@@ -89,6 +165,14 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.outputEncoding = THREE.sRGBEncoding
+
+debugObject.clearColor = '#251320'
+gui.addColor(debugObject, 'clearColor').onChange( ()=>
+{
+    renderer.setClearColor(debugObject.clearColor)
+})
+renderer.setClearColor(debugObject.clearColor)
 
 /**
  * Animate
